4636954089626d82b6407fea1cb5469a314711df
Mayank <mayanklucky4@gmail.com>
2025-09-11 22:36:21 +0530

    Included the Newsletter and Footer section

---
+++ Frontend/src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/30">
+      <div>
+        {/* You can add footer links, logo, or social icons here later */}
+      </div>
+      <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
+        Copyright © 2025 QuickB10g GreatStack - All Rights Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Header from '../Components/header'
 import Bloglist from '../Components/Bloglist'
+import Newsletter from '../Components/Newsletter'
+import Footer from '../Components/Footer'
 const Home = () => {
   return (
     <>
     <Navbar/>
     <Header/>
     <Bloglist/>
+    <Newsletter/>
+    <Footer/>
     </>
   )
 }
