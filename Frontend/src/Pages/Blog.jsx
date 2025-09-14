659f92dc1fdda5c2164bc5cebf8d2c7faa83443a
sam1dh <143286740+sam1dh@users.noreply.github.com>
2025-09-14 13:19:09 +0530

    Blog card

--- Frontend/src/Pages/Blog.jsx
+++ Frontend/src/Pages/Blog.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React, { use, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+// import { useEffect } from 'react';
+import { blog_data } from '../Assets/assets';
 const Blog = () => {
-  return (
+  const { id } = useParams();
+  const [data, setDate] = useState(null);
+  const fetchblogData = async () => {
+    const data = blog_data.find(item => item._id === id);
+    setDate(data);
+  }
+useEffect (()=>{
+    fetchblogData();
+},[])
+  return data ? (
     <div>Blog</div>
-  )
+   ) : <div>Loading...</div>
+  
 }
 
 export default Blog
