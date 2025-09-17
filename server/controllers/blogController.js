b5a6a2194fe9e738b1ecf6697907f46c43ea3289
Mayank <mayanklucky4@gmail.com>
2025-09-17 22:18:18 +0530

    Created an API to add a new Blog

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,3 +1,7 @@
+import fs from 'fs'
+import imagekit from '../configs/imageKit.js';
+import Blog from '../models/Blog.js';
+
 export const addBlog = async (req, res) => {
     try {
         const { title, subTitle, description, category, isPublished } = JSON.parse
@@ -8,6 +12,32 @@ export const addBlog = async (req, res) => {
         if (!title || !description || !category || !imageFile) {
             return res.json({ success: false, message: "Missing required fields" })
         }
+
+        const fileBuffer = fs.readFileSync(imageFile.path)
+
+        // Upload Image to ImageKit
+        const response = await imagekit.upload({
+        file: fileBuffer,
+        fileName: imageFile.originalname,
+        folder: "/blogs"
+    })
+
+    // optimization through imagekit URL transformation
+    const optimizedImageUrl = imagekit.url({
+        path: response.filePath,
+        transformation: [
+            { quality: 'auto' }, // Auto compression
+            { format: 'webp' }, // Convert to modern format
+            { width: '1280' } // Width resizing
+        ]
+    });
+
+        const image = optimizedImageUrl;
+        await Blog.create({title, subTitle, description, category, image, isPublished})
+
+        res.json({success: true, message: "Blog added successfully"})
+
     } catch (error) {
+    res.json({success: false, message: error.message})
     }
 }
