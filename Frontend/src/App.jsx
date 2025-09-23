39eb92ebacde877c249407ba8a8ca9ca5e11b2cd
Mayank <mayanklucky4@gmail.com>
2025-09-23 19:44:12 +0530

    Connected Backend APIs with Frontend Admin Login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,13 +9,19 @@ import ListBlog from './Pages/admin/ListBlog'
 import Comments from './Pages/admin/Comments'
 import Login from './Components/admin/Login';
 import 'quill/dist/quill.snow.css';
+import {Toaster} from 'react-hot-toast'
+import { useAppContext } from './context/AppContext';
+
 const App = () => {
+    
+    const {token} = useAppContext()
   return (
     <div>
+      <Toaster/> 
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/blog/:id' element={<Blog />} />
-        <Route path='/admin' element={true ? <Layout/> : <Login />} >
+        <Route path='/admin' element={token ? <Layout/> : <Login />} >
           <Route index element={<Dashboard />} />
           <Route path='addBlog' element={<AddBlog />} />
           <Route path='listBlog' element={<ListBlog />} />
--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { assets } from '../Assets/assets'
 import { useNavigate } from 'react-router-dom'
-
+import { useAppContext } from '../context/AppContext'
 const Navbar = () => {
-     const navigate = useNavigate();
+
+     const {navigate, token} = useAppContext();
   return (
    
     <div className='flex justify-between items-center py-5 mx-10 sm:mx-20 xl:mx-32'>
         <img onClick={()=>navigate('/')}src={assets.logo} alt="logo" className='w-32 sm:w-48  cursor-pointer' />
-        <button onClick={()=>navigate('/admin')}className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-3'>Login
-            <img src={assets.arrow} className ='w-3' alt="arrow" />
+        <button onClick={()=>navigate('/admin')}className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-3'>
+          {token ? 'Dashboard' : 'Login'}
+          <img src={assets.arrow} className ='w-3' alt="arrow" />
         </button>
     </div>
   )
